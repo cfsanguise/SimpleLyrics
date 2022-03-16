9aded7c5445f6f7ce973c339cb1c9e7939dbdd48
CF Sanguise <estersund@gmail.com>
2022-03-16 23:23:02 +0200

    Fully redesigned

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const fs = require('fs')
 const express = require('express')
 const getLyrics = require('./getLyrics')
 const getSongInfo = require('./getSongInfo')
+const getVideo = require('./getVideo')
 const path = require('path')
 const search = require('./search')
 const public = path.join(__dirname, '../public')
@@ -9,8 +10,7 @@ const html = path.join(public, '/index.html')
 const pageDoesntExist = path.join(public, './404.html')
 
 const server = express()
-const port = process.env.PORT || 3000
-
+const port = proccess.env.PORT || 3000
 server.use(express.static(public))
 
 server.get('/', (req, res) => {
@@ -33,7 +33,6 @@ server.get('/lyrics', (req, res) => {
     const query = req.query.path
     const id = req.query.id
     getLyrics(query).then(result => getSongInfo(id).then(secondResult => res.send({html: result, info:secondResult}))).catch(error => res.status(500).send())
-   
 })
 
 server.use((req, res, next) => {
